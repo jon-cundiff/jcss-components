aa4cf2665db1e4c7367a1aee873f1ec795a3e3fb
jon-cundiff <jon.cundiff@gmail.com>
2022-03-10 17:23:28 -0600

    builds out Button component

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,5 +1,9 @@
 import { MouseEventHandler } from "react";
 export interface ButtonProps {
     text?: string,
-    onClick?: MouseEventHandler<HTMLButtonElement>
+    onClick?: MouseEventHandler<HTMLButtonElement>,
+    faIcon?: string,
+    iconCircle?: boolean,
+    styleType?: "primary" | "secondary" | "danger" | "success" | "info",
+    modifiers?: string[]
 }
