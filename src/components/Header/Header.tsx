0fc466e32c63956fb7216f7d1224d0545985f089
jon-cundiff <jon.cundiff@gmail.com>
2022-03-12 18:25:34 -0600

    adds mobile header toggle. bumps to version 0.1.0

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import ClassNameBuilder from "../Util/ClassNameBuilder";
 import { HeaderProps } from "./Header.types";
@@ -13,6 +14,20 @@ const Header: FC<HeaderProps> = ({
     rightChildren,
     className,
 }) => {
+    const [active, setActive] = useState(false);
+
+    const handleToggle = () => {
+        setActive(!active);
+    };
+
+    const itemsClasses = new ClassNameBuilder("header-items");
+    const toggleClasses = new ClassNameBuilder("mobile-toggle");
+
+    if (active) {
+        itemsClasses.add("active");
+        toggleClasses.add("active");
+    }
+
     const classes = new ClassNameBuilder("header")
         .processProps({ styleType, lighter })
         .processClassName(className);
@@ -45,10 +60,18 @@ const Header: FC<HeaderProps> = ({
                     {logoTextItem}
                 </Link>
             </div>
-            <div className="header-items">
+            <div className={itemsClasses.getClassString()}>
                 {leftChildrenItems}
                 {rightChildrenItems}
             </div>
+            <button
+                className={toggleClasses.getClassString()}
+                onClick={handleToggle}
+            >
+                <span className="toggle-box">
+                    <span className="toggle-inner"></span>
+                </span>
+            </button>
         </header>
     );
 };
