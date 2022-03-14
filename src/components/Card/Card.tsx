8fa0dfb6de7a7c2e01b72592949ca56f497c9a9c
jon-cundiff <jon.cundiff@gmail.com>
2022-03-14 14:22:08 -0500

    implements Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from "react";
+import ClassNameBuilder from "../Util/ClassNameBuilder";
+import { CardProps } from "./Card.types";
+
+const Card: FC<CardProps> = ({
+    title,
+    styleType,
+    lighter,
+    sub,
+    className,
+    innerClassName,
+    children,
+}) => {
+    let mainClasses = new ClassNameBuilder("card")
+        .add(sub ? "sub" : "")
+        .processClassName(className);
+
+    let titleClasses = new ClassNameBuilder("card-title").processProps({
+        styleType,
+        lighter,
+    });
+
+    let bodyClasses = new ClassNameBuilder("card-body").processClassName(
+        innerClassName
+    );
+    return (
+        <div className={mainClasses.getClassString()}>
+            <div className={titleClasses.getClassString()}>{title}</div>
+            <div className={bodyClasses.getClassString()}>{children}</div>
+        </div>
+    );
+};
+
+export default Card;
