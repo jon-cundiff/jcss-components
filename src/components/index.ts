72d8436371098343cce91397ae6472836fafb3a9
jon-cundiff <jon.cundiff@gmail.com>
2022-03-14 14:07:25 -0500

    implements Alert component

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as MobileRow } from "./MobileRow";
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Button } from "./Button";
+export { default as Alert } from "./Alert";
